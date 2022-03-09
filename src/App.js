2efe65548c0ac880fcfa3e12cf9a263be5996930
jawdinmorris <jordanandrewmorris1@gmail.com>
2022-03-09 14:24:02 +1000

    filterButton component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
 function App(props) {
   const taskList = props.tasks.map((task) => (
@@ -16,25 +17,12 @@ function App(props) {
       <h1>Done That</h1>
       <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>All</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton name="Active" pressed={false} id="activeFilter"/>
+        <FilterButton name="All" pressed={true} id="allFilter"/>
+        <FilterButton name="Completed" pressed={false} id="completedFilter" />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
---
+++ src/components/FilterButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FilterButton(props) {
+  return (
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.pressed}
+      id={props.id}
+    >
+      <span className="visually-hidden">Show </span>
+      <span>{props.name}</span>
+      <span className="visually-hidden"> tasks</span>
+    </button>
+  );
+}
