889b097ff0f43de8da49a434007cb347284a7ea1
jawdinmorris <jordanandrewmorris1@gmail.com>
2022-03-09 14:13:35 +1000

    iterative Todo Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Todo from "./components/Todo";
 
 function App(props) {
+  const taskList = props.tasks.map(task => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>Done That</h1>
@@ -44,9 +48,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name ="Eat" completed={true} id="todo-0"/>
-        <Todo name = "Sleep" completed={false} id="todo-1"/>
-        <Todo name = "Garden" completed={false} id="todo-2"/>
+        {taskList}
       </ul>
     </div>
   );
